5135f668cde2f7361ddf8813e8558173544566b6
Ayaan <53174223+astro648@users.noreply.github.com>
2023-10-08 18:54:01 -0700

    write script

--- 06 Pushbutton and LED/Pushbutton-and-LED-Script/Pushbutton-and-LED-Script.ino
+++ 06 Pushbutton and LED/Pushbutton-and-LED-Script/Pushbutton-and-LED-Script.ino
@@ -1,9 +1,17 @@
+#define BUTTON_PIN 2
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(BUTTON_PIN, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(digitalRead(BUTTON_PIN) == HIGH){
+    Serial.println("Button is pressed!");
+  } else {
+    Serial.println("Button is not pressed.");
+  }
+  delay(100); 
 }
