68ef5757282302c9140705506ab6d0bdce5bda0f
Ayaan <53174223+astro648@users.noreply.github.com>
2023-10-08 19:13:48 -0700

    activate LED

--- 06 Pushbutton and LED/Pushbutton-and-LED-Script/Pushbutton-and-LED-Script.ino
+++ 06 Pushbutton and LED/Pushbutton-and-LED-Script/Pushbutton-and-LED-Script.ino
@@ -1,17 +1,18 @@
+#define LED_PIN 11
 #define BUTTON_PIN 2
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if(digitalRead(BUTTON_PIN) == HIGH){
-    Serial.println("Button is pressed!");
+    digitalWrite(LED_PIN, HIGH);
   } else {
-    Serial.println("Button is not pressed.");
+    digitalWrite(LED_PIN, LOW);
   }
   delay(100); 
 }
